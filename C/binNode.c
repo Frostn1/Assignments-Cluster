878f35257d572c8487df924633977f168e31c1aa
Sean Dahan <aui.svi@gmail.com>
2022-01-25 19:43:05 +0200

    Added base C files

---
+++ C/binNode.c
@@ -0,0 +1,122 @@
+#include "binNode.h"
+
+// Addition functions
+BinNode* Add(BinNode* node, int x) {
+    while(node->prev != NULL) node = node->prev;
+    node->prev = newNode(x, NULL, node);
+    return node->prev;
+}
+
+BinNode* Insert(BinNode* node, int pos, int x) {
+    if(pos == NULL_POS) return Add(node, x);
+    int i = 0;
+    while(node->prev != NULL) node = node->prev;
+    while(i++ <= pos && node->next != NULL) node = node->next;
+    if(node->next == NULL) {
+        node->next = newNode(x, node, NULL);
+        return node;
+    }
+
+    node->next = newNode(x, node, node->next->next);
+    return node;
+}
+
+BinNode* InsertSorted(BinNode* node, int x) {
+    while(node->prev != NULL) node = node->prev;
+    while(node->next != NULL && node->data < x) node = node->next;
+    node->next = newNode(x, node, node->next);
+    return node;
+}
+
+// Creation functions
+BinNode* newNode(int data, BinNode* prev, BinNode* next) {
+    BinNode* node = (BinNode*)malloc(sizeof(BinNode));
+    node->data = data;
+    node->prev = prev;
+    node->next = next;
+    return node;
+}
+
+BinNode* Build(int* arr, int size) {
+    if (!arr || !size) return NULL;
+    BinNode* node = newNode(arr[0], NULL, NULL);
+    for (size_t i = 1; i < size; i++) {
+        node->prev = newNode(arr[i], NULL, node);
+        node = node->prev;
+    }
+    return node;
+    
+}
+
+// Removing functions
+BinNode* Remove(BinNode* node) {
+    while(node->prev) node = node->prev;
+    node = node->next;
+    FreeNode(node->prev);
+    node->prev = NULL;
+    return node;
+}
+
+BinNode* RemoveAt(BinNode* node, BinNode* pos) {
+    while(node->prev) node = node->prev;
+    while(node->next && node->next->data != pos->data);
+    if(node->next->data == pos->data) {
+        BinNode* temp = node->next;
+        node->next = node->next->next;
+        node->next->prev = node;
+        FreeNode(temp);
+        return node;
+    } 
+    return NULL;
+}
+
+// Printing functions
+void Show(BinNode* node) {
+    while(node->prev) node = node->prev;
+    while(node) {
+        printf("%d ",node->data);
+        node = node->next;
+    }
+    return;
+}
+
+void ShowBackword(BinNode* node) {
+    while(node->next) node = node->next;
+    while(node) {
+        printf("%d ",node->data);
+        node = node->prev;
+    }
+    return;
+}
+
+// Locating functions
+BinNode* GetLast(BinNode* node) {
+    while(node->next) node = node->next;
+    return node;
+}
+
+BinNode* GetPosition(BinNode* node, int x) {
+    while(node->prev) node = node->prev;
+    while(node->next && node->next->data != x) node = node->next;
+    if(node->next->data == x) return node->next;
+    return NULL;
+}
+
+
+// Utility functions
+void FreeNode(BinNode* node) {
+    node->next = NULL;
+    node->prev = NULL;
+    free(node);
+    return;
+}
+
+void FreeNodeList(BinNode* node) {
+    while(node->prev) node = node->prev;
+    while(node->next) {
+        node = node->next;
+        FreeNode(node->prev);
+    }
+    FreeNode(node);
+    return;
+}
---
+++ C/binNode.h
@@ -0,0 +1,43 @@
+#ifndef BIN_NODE_H
+#define BIN_NODE_H
+
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NULL_POS -1
+
+// Struct for two sided node
+typedef struct BinNode {
+    int data;
+    struct BinNode* next;
+    struct BinNode* prev;
+}BinNode;
+
+// Addition functions
+BinNode* Add(BinNode* node, int x);
+BinNode* Insert(BinNode* node, int pos, int x); // if @pos is null, @x will be added at the end of the list
+BinNode* InsertSorted(BinNode* node, int x);
+
+// Creation functions
+BinNode* newNode(int data);
+BinNode* Build(int* arr, int size); // if @arr is null, an empty list will be returned
+
+// Removing functions
+BinNode* Remove(BinNode* node); // deletes the first element in the list
+BinNode* RemoveAt(BinNode* node, BinNode* pos); // assumption, @pos isn't null
+
+// Printing functions
+void Show(BinNode* node);
+void ShowBackword(BinNode* node);
+
+// Locating functions
+BinNode* GetLast(BinNode* node);
+BinNode* GetPosition(BinNode* node, int x);
+
+
+// Utility functions
+void FreeNode(BinNode* node);
+void FreeNodeList(BinNode* node); // Given node head free entire list forward
+
+#endif // !BIN_NODE_H
